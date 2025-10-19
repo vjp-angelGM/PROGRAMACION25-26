5fc8c7bc6ed6900b9602c1e4856e28a1444b7ac6
vjp-angelGM <121127297+vjp-angelGM@users.noreply.github.com>
2025-10-19 21:47:20 +0200

    Update ContraseñaUsuario.java

--- TEMA 3 Programación/contraseñaUsuario/src/contraseñausuario/ContraseñaUsuario.java
+++ TEMA 3 Programación/contraseñaUsuario/src/contraseñausuario/ContraseñaUsuario.java
@@ -4,7 +4,7 @@
  */
 package contraseñausuario;
 
-import java.util.Scanner;
+import java.util.Scanner; // Importamos la libreria Scanner
 
 /**
  *
@@ -12,30 +12,30 @@ import java.util.Scanner;
  */
 public class ContraseñaUsuario {
 
-    final static int CONTRASEÑA = 1234;
+    final static int CONTRASEÑA = 1234; // Declaramos la contraseña como constante
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        int errores = 0;
-        int clave;
+        Scanner entrada = new Scanner(System.in); // Declaramos el Scanner
+        int errores = 0; // Declaramos los errores
+        int clave; // Declaramos una variable para la clave que será la que se recoja del usuario
         do {
-            System.out.println("Introduce la contraseña: ");
-            clave = entrada.nextInt();
+            System.out.println("Introduce la contraseña: "); // Pedimos al usuario que introduzca una contraseña
+            clave = entrada.nextInt(); // Recogemos la contraseña
 
-            if (clave == CONTRASEÑA) {
-                System.out.println("\n La contraseña es correcta. Enhorabuena, puedes acceder al sistema");
-            } else {
-                errores++;
-                System.out.println("\n Contraseña incorrecta");
-                System.out.println("--> Intentos restantes: " + (3 - errores));
+            if (clave == CONTRASEÑA) { // Comparamos lo que ha introducido el usuario con lo almacenado
+                System.out.println("\n La contraseña es correcta. Enhorabuena, puedes acceder al sistema"); // Si coincide mostramos este mensaje
+            } else { // Si no coincide
+                errores++;  // Añadimos un error
+                System.out.println("\n Contraseña incorrecta"); // Mostramos este mensaje
+                System.out.println("--> Intentos restantes: " + (3 - errores)); // Mostramos el numero de intentos restantes 
             }
-        } while (clave != CONTRASEÑA && errores < 3);
+        } while (clave != CONTRASEÑA && errores < 3); // Repetir mientras la clave sea distinta y el numero de intentos no sea igual a 3
 
-        if (errores == 3 && clave != CONTRASEÑA) {
-            System.out.println("\n Lo siento, has alcanzado el límite de intentos de acceso al sistema. ");
+        if (errores == 3 && clave != CONTRASEÑA) { // Si el numero de errores es 3 y la clave es distinta a la contraseña 
+            System.out.println("\n Lo siento, has alcanzado el límite de intentos de acceso al sistema. "); // Mostramos este mensaje
         }
     }
 
