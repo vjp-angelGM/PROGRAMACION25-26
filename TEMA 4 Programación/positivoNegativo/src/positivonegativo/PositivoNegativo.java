dda5b8e255bab1aab4716e17ba0de61082133a83
vjp-angelGM <121127297+vjp-angelGM@users.noreply.github.com>
2025-10-24 19:37:31 +0200

    Ejercicio 1 Tema 4

---
+++ TEMA 4 Programación/positivoNegativo/src/positivonegativo/PositivoNegativo.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package positivonegativo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author angel
+ */
+public class PositivoNegativo {
+
+    public static int PedirNum(int n1) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Por favor introduce un número: ");
+        n1 = entrada.nextInt();
+        return n1;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n1 = PositivoNegativo.PedirNum(0);
+
+        if (n1 > 0) {
+            System.out.println("El numero es positivo");
+        } else if (n1 < 0) {
+            System.out.println("El numero es negativo");
+        } else {
+            System.out.println("El numero es 0");
+        }
+    }
+
+}
