e9e15be4075539aab56a380fc3c864c8440aa7c7
vjp-angelGM <121127297+vjp-angelGM@users.noreply.github.com>
2025-11-04 23:11:42 +0100

    Ejercicio 7 Corregido

---
+++ TEMA 4 Programación/numerosCreciente/src/numeroscreciente/Burbuja.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package numeroscreciente;
+
+/**
+ *
+ * @author angel
+ */
+public class Burbuja {
+
+    public static void ordenarBurbuja(int n1, int n2, int n3, int n4) {
+        int aux;
+        for (int i = 1; i <= 3; i++) {
+            if (n1 > n2) {
+                aux = n1;
+                n1 = n2;
+                n2 = aux;
+            }
+            if (n2 > n3) {
+                aux = n2;
+                n2 = n3;
+                n3 = aux;
+            }
+            if (n3 > n4) {
+                aux = n3;
+                n3 = n4;
+                n4 = aux;
+            }
+        }
+
+        System.out.println("Números en orden creciente: " + n1 + " - " + n2 + " - " + n3 + " - " + n4);
+    }
+}
