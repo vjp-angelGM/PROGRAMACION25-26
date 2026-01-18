e2d39c665eb7b76b12268f8ac0543a95e0c65947
vjp-angelGM <121127297+vjp-angelGM@users.noreply.github.com>
2026-01-18 15:31:03 +0100

    Comentario del ejercicio 4

--- TEMA 7 Programación/Ejercicio4T7/src/ejercicio4t7/Ejercicio4T7.java
+++ TEMA 7 Programación/Ejercicio4T7/src/ejercicio4t7/Ejercicio4T7.java
@@ -13,18 +13,24 @@ public class Ejercicio4T7 {
     /**
      * @param args the command line arguments
      */
+    //Método que genera el array con números aleatorios
     public static void numerosAleatorios(int[][] array) {
         System.out.println("Generando el array con numeros aleatorios: ");
+        //Recorremos las filas del array
         for (int i = 0; i < array.length; i++) {
+            //Recorremos las columnas del array
             for (int j = 0; j < array[i].length; j++) {
                 array[i][j] = (int) (Math.random() * (200 - 100) + 100);
             }
         }
     }
 
+    //Método que muestra los números pares 
     public static void mostrarPares(int[][] array) {
         System.out.println("Mostrando numeros pares: ");
+        //Recorremos las filas del array
         for (int i = 0; i < array.length; i++) {
+            //Recorremos las columnas del array
             for (int j = 0; j < array[i].length; j++) {
                 if (array[i][j] % 2 == 0) {
                     System.out.print(array[i][j]);
